2fb0b57f7c33088da669ca1481ea1160e5252722
MajkeM <156318301+MajkeM@users.noreply.github.com>
2025-09-17 11:21:16 +0200

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // důležité pro static export
+  images: {
+    unoptimized: true, // GitHub Pages neumí optimalizaci obrázků
+  },
+  basePath: "/DHgravirovani", // tady dej název svého repa
+  assetPrefix: "/DHgravirovani/",
 };
 
 export default nextConfig;
