66cdb136b97ea77a9a77765c2e415d2c583f660d
MajkeM <156318301+MajkeM@users.noreply.github.com>
2025-09-22 19:32:37 +0200

    icon and details

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <div className="FOOTER">
-                <p>© 2025 dHgravirovani.cz | všechna práva vyhrazena | vytvořil <a rel="noopener noreferrer" target="_blank" href="https://www.michael-micin.space">Michael Mičín</a> </p>
+                <p>© 2025 dHgravirovani.cz | všechna práva vyhrazena | vytvořil Michael Mičín. Chcete podobnou stránku? Klikněte <a rel="noopener noreferrer" target="_blank" href="https://www.michael-micin.space">sem</a> </p>
         </div>
     )
 }
